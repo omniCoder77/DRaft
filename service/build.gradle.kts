6f531673f076a8fa8e90d17370ca4f64ed27fb47
omniCoder77 <rishabhsaraswat17@gmail.com>
2026-01-16 13:04:19 +0530

    fixed main not found issue

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    kotlin("jvm") version "2.2.21"
+    kotlin("jvm") version "2.0.21"
     application
+    id("com.gradleup.shadow") version "8.3.5"
 }
 
 group = "com.ethyllium"
@@ -13,14 +14,12 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("jakarta.servlet:jakarta.servlet-api:5.0.0")
-
     implementation("org.eclipse.jetty:jetty-server:11.0.24")
     implementation("org.eclipse.jetty:jetty-servlet:11.0.24")
-
 }
 
 application {
-    mainClass.set("com.ethyllium.Mainkt")
+    mainClass.set("com.ethyllium.MainKt")
 }
 
 kotlin {
