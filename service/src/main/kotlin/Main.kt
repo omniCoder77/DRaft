ecce4111c354e6072f6c2a839c8e046e56297e53
omniCoder77 <rishabhsaraswat17@gmail.com>
2026-01-16 12:40:44 +0530

    commit

--- service/src/main/kotlin/Main.kt
+++ service/src/main/kotlin/Main.kt
@@ -4,21 +4,16 @@ import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.servlet.ServletContextHandler
 import org.eclipse.jetty.servlet.ServletHolder
 
+fun main() {
+    val port = System.getenv("PORT")?.toInt() ?: throw RuntimeException("PORT environment variable not set")
+    val server = Server(port)
 
-object MainApp {
-    @Throws(Exception::class)
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val port = System.getenv("PORT")?.toInt() ?: throw RuntimeException("PORT environment variable not set")
-        val server = Server(port)
+    val context = ServletContextHandler(ServletContextHandler.SESSIONS)
+    context.contextPath = "/"
+    server.handler = context
 
-        val context = ServletContextHandler(ServletContextHandler.SESSIONS)
-        context.contextPath = "/"
-        server.handler = context
+    context.addServlet(ServletHolder(SimpleServlet()), "/simple")
 
-        context.addServlet(ServletHolder(SimpleServlet()), "/simple")
-
-        server.start()
-        server.join()
-    }
+    server.start()
+    server.join()
 }
