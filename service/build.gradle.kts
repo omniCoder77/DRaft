94a975bd90a83fd30f956f9e551a77236263ec6b
omniCoder77 <rishabhsaraswat17@gmail.com>
2026-01-23 16:07:41 +0530

    improved ci

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     kotlin("jvm") version "2.0.21"
-    application
     id("com.gradleup.shadow") version "8.3.5"
 }
 
@@ -17,13 +16,9 @@ dependencies {
     implementation("org.eclipse.jetty:jetty-server:11.0.24")
     implementation("org.eclipse.jetty:jetty-servlet:11.0.24")
     implementation ("com.ethyllium:grpc-service:1.0.0")
-
     testImplementation("io.mockk:mockk:1.14.7")
 }
 
-application {
-    mainClass.set("com.ethyllium.MainKt")
-}
 
 kotlin {
     jvmToolchain(21)
